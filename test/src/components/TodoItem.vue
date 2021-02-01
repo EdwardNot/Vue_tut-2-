6c42e5dedadc96b90e876fc69dd000271ad91a79
Edward Not <leonardovan@yandex.ru>
2021-02-01 13:17:30 +0300

    Inti commit

---
+++ test/src/components/TodoItem.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="todo-item" :class="{'is-competed': todo.status}">
+        <p> 
+            <input type="checkbox" v-on:change="markComplete"> 
+            {{todo.name}}
+            <button @click="$emit('del-todo', todo.id)" class = "del">x</button>
+            </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "TodoItem",
+    props: [
+        "todo"
+    ],
+    methods: {
+        markComplete(){
+            this.todo.status = !this.todo.status;
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .todo-item {
+        background: #f4f4f4;
+        padding: 10px;
+        border-bottom: 1px #ccc dotted;
+    }
+
+    .is-competed {
+        text-decoration: line-through;
+    }
+
+    .del {
+        background: #ff0000;
+        color: #fff;
+        border: none;
+        padding: 5px 9px;
+        border-radius: 50%;
+        cursor: pointer;
+        float: right;
+    }
+</style>
